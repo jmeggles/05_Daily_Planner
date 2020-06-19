f072dc09a7321f710ec2baae9c629c387b944fbb
Jody Eggleston <jmeggles@gmail.com>
2020-06-19 09:53:52 -0400

    added console.logs back in.

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,7 +1,11 @@
+// jquery document.ready
+
 // GIVEN I am using a daily planner to create a schedule
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
-const NOW = moment();
+const NOW = moment()
+$("#currentDay").text(moment().format('MMMM Do YYYY'))
+console.log("time");
 var schedule = {
     '14': ""
 };
@@ -10,27 +14,34 @@ var schedule = {
 // THEN I am presented with timeblocks for standard business hours
 function buildRow(hour) {
     var time = moment(hour, "H");
+    console.log("hour", "H");
 // THEN each timeblock is color coded to indicate it is in the future    
     var timeClass = "future";
     var task = schedule[hour];
+    console.log("future")
 
     
 //timeblock is color coded to indicate it is in the present
     if (time.isSame(NOW, "hour")) {
         timeClass = "present"
+        console.log("present");
     }
 
 // timeblock is color coded to indicate it is in the past
     else if (time.isBefore(NOW)) {
         timeClass = "past"
+        console.log("past");
     }
 
     var parentEl = $("<div>")
     parentEl.addClass("row time-block")
+    console.log("row time-block")
 
     var hourEl = $("<div>")
     hourEl.addClass("col-1 hour")
     hourEl.text(time.format("hA"))
+    console.log("col-1 hour");
+    console.log("ha")
 
     // THEN the text for that event is saved in local storage
     var textEl = $("<textarea>")
@@ -43,16 +54,19 @@ function buildRow(hour) {
     // adding save button from fontawesome
     var icon = $("<i>")
     icon.addClass("fa fa-save")
-    
+    console.log("added save icon");
+
     // applying save button 
     btn.append(icon)
     parentEl.append(hourEl, textEl, btn)
+    console.log("save button");
 
     $(".container").append(parentEl)
 }
-// adding all rows for the dsy
+// adding all rows for the day
 for (var i = 8; i <= 18; i++) {
     buildRow(i)
+    console.log("each row");
 }
 
 
